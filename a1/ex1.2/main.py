448fe28653bdae42be13d47e95a81d61ce0463cd
Tieg <tiegosullivanpsnl@gmail.com>
2020-09-09 11:18:45 +0200

    :sparkles: ex1.2, ex1.3, ex1.4

---
+++ a1/ex1.2/main.py
@@ -0,0 +1,38 @@
+class RGBColour:
+    def __init__(self, r, g, b):
+        if r > 255 or r < 0 or g > 255 or g < 0 or b > 255 or b < 0:
+            raise ValueError("out of range [0,255]")
+        self.r = r
+        self.g = g
+        self.b = b
+    
+    # equality
+    def __eq__(self, other):
+        if isinstance(other, RGBColour):
+            return self.r == other.r and self.g == other.g and self.b == other.b
+        return False
+    
+    # getters
+    def red(self):
+        return self.r
+    def green(self):
+        return self.g
+    def blue(self):
+        return self.b
+    
+    # functions
+    def luminance(self):
+        return 0.299*self.r + 0.587*self.g + 0.114*self.b
+    
+    # toString
+    def __str__(self):
+        return '(%s, %s, %s) = %s' % (str(self.red()), str(self.green()), str(self.blue()), str(self.luminance()))
+
+# testing functions
+c = RGBColour(122, 200, 170)
+print(c)
+
+# testing equality
+c1 = RGBColour(122, 200, 171)
+c2 = RGBColour(122, 200, 170)
+print(c1 == c2)
