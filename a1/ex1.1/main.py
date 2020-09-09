4bbb8f1e5a7768952eab121a1ff838d869796ea4
Tieg <tiegosullivanpsnl@gmail.com>
2020-09-09 09:51:23 +0200

    :sparkles: ex1.1

---
+++ a1/ex1.1/main.py
@@ -0,0 +1,22 @@
+import math
+class Charge:
+    def __init__(self, q, x, y):
+        self.q = q
+        self.x = x
+        self.y = y
+    
+    def __str__(self):
+        return "%s @ (%s, %s)" % (str(self.q), str(self.x), str(self.y))
+    
+    def distance(self, x, y):
+        return math.sqrt((self.x-x)*(self.x-x)) + math.sqrt((self.y*y)*(self.y*y))
+    
+    def potential_at(self, x, y):
+        r = self.distance(x, y)
+        q = self.q
+        k = 8.99 * 10 ** 9
+        return k*q/r
+
+q1 = Charge(2.2, 2.4, 2.6)
+print(q1)
+print(q1.potential_at(3.4, 1.2))
