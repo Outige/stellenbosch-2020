0d72b79f1f6f3aba29aaa914e3fca7825a8ff870
Tieg <tiegosullivanpsnl@gmail.com>
2021-03-29 20:05:07 +0200

    :sparkles: ops example

---
+++ or322/17.5/example0.r
@@ -0,0 +1,26 @@
+# S={0, 1, 2} represents the number of working machines at the beggining of the day
+P=matrix(c(
+    0.0, 0.0, 1.0,
+    0.0, 1/3, 2/3,
+    1/9, 4/9, 4/9
+), nrow=3, byrow=TRUE)
+colnames(P) = c('0', '1', '2')
+rownames(P) = c('0', '1', '2')
+
+# Print P
+P
+
+# 1st we modify P. P = P - I
+PMod = P - diag(3)
+
+# Replace any column with 1s. We choose the last one here
+PMod[,3] = 1
+PMod
+
+# Invert PMod
+PModInv = solve(PMod)
+PModInv
+
+# Calculate pibar. 0s except for the column of PMod that is all 1s
+pibar = c(0, 0, 1)%*%PModInv
+pibar
