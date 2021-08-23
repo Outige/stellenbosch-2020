69fceea0c0552921da30ec8ffccbad615e76524c
Tieg O'Sullivan <tiegosullivanpsnl@gmail.com>
2021-08-23 13:17:00 +0200

    :sparkles::construction: T07 complete - very scuffed 20.10

--- or322/a2/w7/t07_a2_prep.R
+++ or322/a2/w7/t07_a2_prep.R
@@ -79,7 +79,7 @@ Wq(lambda, muav), Wq(lambda, muav)*60))
 #   Q: ?
 
 # SUMMARY: # TODO
-#   - ?
+#   - customer needs to served by K stations in any order before completion
 
 #--------------------------------------------------
 # PROBLEM 1
@@ -87,6 +87,9 @@ Wq(lambda, muav), Wq(lambda, muav)*60))
 # A Social Security Administration branch is considering
 # the following two options for processing applications for
 # social security cards:
+# The interarrival time of applicants is exponential, and an
+# average of 4.8 applicants arrive each hour
+#   a) Which option will get applicants out of the office more quickly?
 
 # OPTION 1:
 # Three clerks process applications in parallel from
@@ -94,6 +97,42 @@ Wq(lambda, muav), Wq(lambda, muav)*60))
 # cation in the presence of the applicant. Processing time is
 # exponential with a mean of 15 minutes. Interarrival times
 # are exponential.
+
+#---------
+# option 1
+#---------
+# standard 20.6 Q
+
+s = 3
+mu = 1/15*60 # c/h, exponential(M)
+lambda = 4.8 # c/h, exponential(M)
+# Q: M/M/3/GD/∞/∞
+rho6 = function(lambda, mu, s) {
+    return(
+        lambda/(mu*s)
+    )
+}
+
+PI06 = function(lambda, mu, s) {
+    D = 0
+    for (j in 0:(s-1)) {
+        D = D + (s*rho6(lambda, mu, s))^j/factorial(j)
+    }
+    D = D + (s*rho6(lambda, mu, s))^s/(factorial(s)*(1-rho6(lambda, mu, s)))
+    return(1/D)
+}
+
+Pj6 = function(lambda, mu, s) {
+    return(
+        PI06(lambda, mu, s)*( (s*rho6(lambda, mu, s))^s/(factorial(s)*(1-rho6(lambda, mu, s))) )
+    )
+}
+
+W6 = function(lambda, mu, s) {
+    return(Pj6(lambda, mu, s)/(s*mu - lambda) + 1/mu)
+}
+cat(sprintf("20.10: Problem 1: Option 1 is\u001b[36m %f \u001b[0mminutes\n", W6(lambda, mu, s)*60))
+
 
 # OPTION 2:
 # Each applicant first fills out an application with-
@@ -103,6 +142,117 @@ Wq(lambda, muav), Wq(lambda, muav)*60))
 # to wait for one of the three clerks to check the form. It takes
 # a clerk an average of 4 minutes (exponentially distributed)
 # to review an application.
-# The interarrival time of applicants is exponential, and an
-# average of 4.8 applicants arrive each hour
-#   a) Which option will get applicants out of the office more quickly?
+
+#---------
+# option 2
+#---------
+# looks like a self service 20.7 Q into a parallel 20.6 Q, so sum W7 + W6
+s = 3
+lambda = 4.8 # c/h
+mu1 = 1/65*60 # c/h
+mu2 = 1/4*60 # c/h
+
+W7 = function(lambda, mu) {
+    return(1/mu)
+}
+cat(sprintf("20.10: Problem 1: Option 2 is\u001b[36m %f \u001b[0mminutes\n\n", W6(lambda, mu2, s)*60 + W7(lambda, mu1)*60))
+
+
+#--------------------------------------------------
+# PROBLEM 6
+#--------------------------------------------------
+# Consider a queuing system consisting of three stations
+# in series. Each station consists of a single server, who can
+# process an average of 20 jobs per hour (processing times at
+# each station are exponential). An average of 10 jobs per
+# hour arrive (interarrival times are exponential) at station 1.
+# When a job completes service at station 2, there is a .1
+# chance that it will return to station 1 and a .9 chance that it
+# will move on to station 3. When a job completes service at
+# station 3, there is a .2 chance that it will return to station 2
+# and a .8 chance that it will leave the system. All jobs
+# completing service at station 1 immediately move on to
+# station 2.
+#     (a) Determine the fraction of time each server is busy.
+#     (b) Determine the expected number of jobs in the
+#     system.
+#     (c) Determine the average time a job spends in the
+#     system.
+
+# NOTE: 3 stations in series. Classic 20.10. lambda is constant
+# lambda = 10 # c/h, M - lambda1 which sets the whole system
+# mu = 20 # c/h, M - all mu
+# x = matrix(c(
+#     0, 1, 0,
+#     0.1, 0, 0.9,
+#     0, 0.2, 0
+# ), nrow=3, byrow=TRUE)
+# x
+# # Q: ?
+
+# 3 stations in series
+# open has multiple lambdas, closed has constant
+# open has multiple Rs, open is like a general version?
+
+rbar = c(
+  10, # jobs/hour
+  0, # jobs/hour
+  0 # jobs/hour
+)
+
+mubar = c(
+  20, # jobs/hour
+  20, # jobs/hour
+  20 # jobs/hour
+)
+
+P = matrix(c(
+  0,   1,   0,
+  0.1, 0,   0.9,
+  0,   0.2, 0
+), nrow=3, byrow=TRUE)
+
+lambdabar = solve(diag(3)-t(P))%*%rbar
+
+#-----
+# a)
+#-----
+# so each each queue is an M/M/1/GD/inf/inf queue (20.4). So any queue specific things like Pi0 etc which is what we have to do here
+Pi0bar = c()
+for (i in 1:3) {
+  rho = lambdabar[i]/mubar[i]
+  Pi0bar[i] = 1-rho
+}
+cat(sprintf("20.10: Problem 6a: The probability that each server is busy is\u001b[36m %s \u001b[0m\u001b[31m* \u001b[0m\n", Pi0bar)) # TODO: Why is it PI0 not 1-PI0?
+
+
+#-----
+# b)
+#-----
+L = function(lambda, mu) {
+  return(lambda/(mu-lambda)) # this formula is out up top
+}
+
+Lbar = c()
+for (i in 1:3) {
+  Lbar[i] = L(lambdabar[i], mubar[i])
+}
+
+Ltau = sum(Lbar)
+
+cat(sprintf("20.10: Problem 6b: The expected number of jobs in the system is\u001b[36m %f \u001b[0mjobs\n", Ltau))
+
+#-----
+# c)
+#-----
+Wtau = Ltau/sum(rbar)
+
+Wtau1 = 0
+
+for (i in 1:3) {
+  Wtau1 = Wtau1 + Lbar[i]/lambdabar[i]
+}
+
+cat(sprintf("20.10: Problem 6c: The average time a job spends in the system is\u001b[36m %f \u001b[0mhours or\u001b[36m %f \u001b[0mminutes\n", Wtau, Wtau*60))
+cat(sprintf("       or is it\u001b[36m %f \u001b[0mhours or\u001b[36m %f \u001b[0mminutes\u001b[0m\u001b[31m* \u001b[0m\n\n", Wtau1, Wtau1*60))
+# FIXME: Wtau or Wtau1, which is best? I think W1 as its dr jacobs
