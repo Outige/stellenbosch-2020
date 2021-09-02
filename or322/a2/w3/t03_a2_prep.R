ad39e6d7e041f308e540f77014e93d4c347dd090
Tieg O'Sullivan <tiegosullivanpsnl@gmail.com>
2021-09-02 10:53:27 +0200

    :sparkles: 17.6, :construction: 17.6 + TT03

---
+++ or322/a2/w3/t03_a2_prep.R
@@ -0,0 +1,170 @@
+#----------------------------------------------------------------------------------------------------
+# SECTION 17.6
+#----------------------------------------------------------------------------------------------------
+
+#--------------------------------------------------
+# PROBLEM 1
+#--------------------------------------------------
+#       (a) If a student enters State College as a freshman, how many years can he expect to spend as a student at State?
+#       (b) What is the probability that a freshman graduates?
+
+# GIVEN
+P = matrix(c(
+    0.10, 0.80, 0.00, 0.00, 0.10, 0.00,
+    0.00, 0.10, 0.85, 0.00, 0.05, 0.00,
+    0.00, 0.00, 0.15, 0.80, 0.05, 0.00,
+    0.00, 0.00, 0.00, 0.10, 0.05, 0.85,
+    0.00, 0.00, 0.00, 0.00, 1.00, 0.00,
+    0.00, 0.00, 0.00, 0.00, 0.00, 1.00
+), nrow=6, byrow=TRUE)
+colnames(P) = rownames(P) = c("F.", "So.", "J.", "Sen.", "Q.", "G.")
+
+Q = P[1:4, 1:4]
+R = P[1:4, 5:6]
+F = solve(diag(4) - Q)
+A = F%*%R
+
+cat(sprintf("17.6: Problem 1a: If a student enters as a freshman they will spend on average \u001b[36m%f\u001b[0m years at the college\n", sum(F[1,])))
+
+cat(sprintf("17.6: Problem 1b: The probability a freshman graduates is \u001b[36m%f\u001b[0m\n\n", A[1, 2]))
+
+#--------------------------------------------------
+# PROBLEM 2
+#--------------------------------------------------
+# The Herald Tribble has obtained the following information about its subscribers:
+# During the first year as subscribers, 20% of all subscribers cancel their
+# subscriptions. Of those who have subscribed for one year, 10% cancel during
+# the second year. Of those who have been subscribing for more than two years,
+# 4% will cancel during any given year.
+#         (a) On the average, how long
+#         does a subscriber subscribe to the Herald Tribble?
+
+P = matrix(c(
+    0.00, 0.80, 0.00, 0.20,
+    0.00, 0.00, 0.90, 0.10,
+    0.00, 0.00, 0.96, 0.04,
+    0.00, 0.00, 0.00, 1.00
+), nrow=4, byrow=TRUE)
+colnames(P) = rownames(P) = c("Y0", "Y1", "Y2+", "C")
+
+Q = P[1:3, 1:3]
+R = P[1:3, 4]
+F = solve(diag(3)-Q)
+A = F%*%R
+
+cat(sprintf("17.6: Problem 2: On average a user subscibes to the herald for \u001b[36m%f\u001b[0m years\n\n", sum(F[1,])))
+
+
+#--------------------------------------------------
+# PROBLEM 5
+#--------------------------------------------------
+# Each week, the number of acceptable-quality units of a drug that
+# are processed by a machine is observed: 100, 50–100,
+# 1–50, 0 (indicating that the machine was broken during the week).
+# Given last week’s observation, the probability distribution
+# of next week’s observation is as follows.
+# For example, if we observe a week in which more than 100 units are
+# produced, then there is a .10 chance that during the next
+# week 50–100 units are produced.
+#       (a) Suppose last week the machine produced 200 units.
+#       On average, how many weeks will elapse before the machine
+#       breaks down?
+#       (b) Suppose last week the machine produced 50 units.
+#       On average, how many weeks will elapse before the machine breaks
+#       down?
+
+# GIVEN
+P = matrix(c(
+    0.80, 0.10, 0.05, 0.05,
+    0.10, 0.60, 0.10, 0.20,
+    0.10, 0.10, 0.50, 0.30,
+    0.00, 0.00, 0.00, 1.00
+), nrow=4, byrow=TRUE)
+colnames(P) = rownames(P) = c(">100", "50-100", "1-50", "0")
+
+Q = P[1:3, 1:3]
+R = P[1:3, 4]
+F = solve(diag(3)-Q)
+A = F%*%R
+
+cat(sprintf("17.6: Problem 5a: On average a machine that produces 200 units will take on average \u001b[36m%f\u001b[0m weeks to breakdown\n", sum(F[1,])))
+
+cat(sprintf("17.6: Problem 5b: On average a machine that produces 50 units will take on average \u001b[36m%f\u001b[0m weeks to breakdown\n\n", sum(F[3,])))
+
+#--------------------------------------------------
+# PROBLEM 12
+#--------------------------------------------------
+cat(sprintf("17.6: Problem 12: \u001b[31mscuffed theory\u001b[0m\n\n"))
+
+
+#--------------------------------------------------
+# PROBLEM 16
+#--------------------------------------------------
+P = matrix(c(
+    1.00, 0.00, 0.00, 0.00, 0.00, 0.00,
+    0.00, 1.00, 0.00, 0.00, 0.00, 0.00,
+    0.10, 0.30, 0.00, 0.25, 0.20, 0.15,
+    0.05, 0.45, 0.00, 0.20, 0.20, 0.10,
+    0.15, 0.10, 0.00, 0.15, 0.25, 0.35,
+    0.20, 0.05, 0.00, 0.15, 0.30, 0.30
+), nrow=6, byrow=TRUE)
+colnames(P) = rownames(P) = c("(1)", "(2)", "(3)", "(4)", "(5)", "(6)")
+
+Q = P[3:6, 3:6]
+R = P[3:6, 1:2]
+F = solve(diag(4)-Q)
+A = F%*%R
+cat(sprintf("17.6: Problem 16a: A new customer will take on average \u001b[36m%f\u001b[0m calls before a sale is made or lost\n", sum(F[1,])))
+cat(sprintf("17.6: Problem 16b: Fraction of new customers who purchase the product is \u001b[36m%f\u001b[0m\n", A[1,1]))
+cat(sprintf("17.6: Problem 16c: Fraction of low interest customers who purchase the product is \u001b[36m%f\u001b[0m\n", A[2,1]))
+
+profit3 =  -sum(F[1,])*15 + A[1,]%*%c(190, 0)
+profit4 =  -sum(F[2,])*15 + A[2,]%*%c(190, 0)
+profit5 =  -sum(F[3,])*15 + A[3,]%*%c(190, 0)
+profit6 =  -sum(F[4,])*15 + A[4,]%*%c(190, 0)
+cat(sprintf("17.6: Problem 16d: Profit of each customer is: \u001b[36m{3: %f, 4: %f, 5: %f, 6: %f}\u001b[0m\n\n", profit3, profit4, profit5, profit6))
+
+
+#----------------------------------------------------------------------------------------------------
+# SECTION 17.7
+#----------------------------------------------------------------------------------------------------
+
+#--------------------------------------------------
+# PROBLEM 1
+#--------------------------------------------------
+# Suuppose that each year state college admits 7000
+# freshman, 500 sophmores, 500 juniors.
+#       (a) What will the long run composition be?
+
+# GIVEN
+P = matrix(c(
+    0.10, 0.80, 0.00, 0.00, 0.10, 0.00,
+    0.00, 0.10, 0.85, 0.00, 0.05, 0.00,
+    0.00, 0.00, 0.15, 0.80, 0.05, 0.00,
+    0.00, 0.00, 0.00, 0.10, 0.05, 0.85,
+    0.00, 0.00, 0.00, 0.00, 1.00, 0.00,
+    0.00, 0.00, 0.00, 0.00, 0.00, 1.00
+), nrow=6, byrow=TRUE)
+colnames(P) = rownames(P) = c("F.", "So.", "J.", "Sen.", "Q.", "G.")
+Q = P[1:4, 1:4]
+
+bbar = c(7000, 500, 500, 0)
+hbar = bbar %*% (diag(4)-Q)
+# hbar
+
+# bbar = hbar %*% solve(diag(4)-Q)
+# rownames(bbar) = c('bbar')
+# bbar
+cat(sprintf("17.7: Problem 1: \u001b[31mhelp\u001b[0m\n\n"))
+
+#--------------------------------------------------
+# PROBLEM 2
+#--------------------------------------------------
+
+#--------------------------------------------------
+# PROBLEM 4
+#--------------------------------------------------
+
+#--------------------------------------------------
+# PROBLEM 7
+#--------------------------------------------------
